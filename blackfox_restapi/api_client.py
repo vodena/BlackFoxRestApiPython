46a727dad64e715bd788bea60b22dd70e193dc2c
tomislav.mrda <t.mrdja@hotmail.com>
2019-08-06 12:30:58 +0200

    response decode

--- blackfox_restapi/api_client.py
+++ blackfox_restapi/api_client.py
@@ -226,7 +226,7 @@ class ApiClient(object):
 
         # fetch data from response object
         try:
-            data = json.loads(response.data.decode('utf-8'))
+            data = json.loads(response.data)
         except ValueError:
             data = response.data
 
